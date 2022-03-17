4fa23c1f4e2398009c4ee1773b3255be07ba2816
Kewin.S <Kewinsp300@gmail.com>
2022-03-17 15:30:29 +0000

    Created Spartan class with setters and validation seperate to ease POST validation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 import json
+from spartan import Spartan
 
 
 app = Flask(__name__)
@@ -29,74 +30,76 @@ def add_employee():
             data_dict.update(employee_data_as_dict_for_json)
             sparta_json.seek(0)
             json.dump(data_dict, sparta_json, indent=3)
-
-    
-        return data_dict
+  
+            return f"Employee {First_name} {Last_name} will be added to the database"
            
     except Exception as ex:
-        print("There was an error")
-        print(ex)
+        return f"There was an error: {ex}"
+        
 
 
 ### Working with correct ID and without valid id ###
 @app.route("/spartan/<spartan_id>", methods = ["GET"])
 def show_spartan(spartan_id):
     spartan_id_str = str(spartan_id)
-
-    if spartan_id_str in data_dict.keys():
-        return data_dict[spartan_id_str]
-    else:
-        return f"There is no Employee with this ID : {spartan_id_str}"
+    try:
+        with open("spartan_data.json", "r+") as sparta_json:
+            data_dict = json.load(sparta_json)
 
 
+        if spartan_id_str in data_dict.keys():
+            return data_dict[spartan_id_str]
+        else:
+            return f"There is no Employee with this ID : {spartan_id_str}"
+
+    except Exception as ex:
+        return f"There was an error: {ex}"
+        
 
 
-#### COME BACK TO THIS POST NOT AUTHORISED ###
+#### WORKING REMOVES EMPLOYEE FROM JSON FUNCTIONAL AND TESTED ###
 @app.route("/spartan/remove", methods = ["POST"])
 def remove_employee():
     iden_variable = request.args.get("sparta_id")
-
-
-
-
     sparta_id_str = str(iden_variable)
 
-    if sparta_id_str in data_dict.keys():
-        del data_dict[sparta_id_str]
-       
+    try:
+        with open("spartan_data.json", "r+") as sparta_json:
+            data_dict = json.load(sparta_json)
+            if sparta_id_str in data_dict.keys():
 
-        try:
-            with open("spartan_data.json", "w") as j:
-                json.dump(data_dict, j, indent=9)
+                data_dict.pop(sparta_id_str)
+               
+                with open("spartan_data.json", "w") as sparta_json:
+                        json.dump(data_dict, sparta_json, indent=3)
 
-        except Exception as ex:
-            return f"There was an {ex} error"
+                return f"Employee with ID: {sparta_id_str} removed from database"   
+            
+            else:
+                return "Cannot remove employee as There is no employee with this ID"
 
-        return "Employee data successfully removed"         
-    else:
-        return "Cannot remove employee as There is no employee with this ID"
-
-   
+    except Exception as ex:
+        return f"There was an error: {ex}"
+        
 
 
 ### Working with hardcoded json ###
 @app.route("/spartan", methods = ["GET"])
 def show_spartan_list_json():
-    json_data = json.dumps(data_dict)
-    return json_data
 
+    try:
+        with open("spartan_data.json", "r+") as sparta_json:
+            data_dict = json.load(sparta_json)
+            return data_dict
+
+    except Exception as ex:
+        return f"There was an error: {ex}"
 
 
 
 ### Start of Main ###
 if __name__ == "__main__":
-
-    try:
-        with open("spartan_data.json", "r+") as sparta_json:
-           data_dict = json.load(sparta_json)
             
         app.run()
 
-    except Exception as ex:
-        print("There was an error")
-        print(ex)
+   
